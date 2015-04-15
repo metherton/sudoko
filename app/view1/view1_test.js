5beec01f11015fca13d426eceeeb6bdbba6d296b
metherton <martinetherton63@googlemail.com>
2015-04-15 22:09:46 +0200

    added unit test

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -2,13 +2,24 @@
 
 describe('myApp.view1 module', function() {
 
+  var $rootScope, $compile;
+
   beforeEach(module('myApp.view1'));
+
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
 
   describe('view1 controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
+
+      var $scope = {};
+      var view1Ctrl = $controller('View1Ctrl', {$scope : $scope});
+      $scope.employees = [];
       expect(view1Ctrl).toBeDefined();
     }));
 
--- app/view2/view2_test.js
+++ app/view2/view2_test.js
@@ -2,14 +2,24 @@
 
 describe('myApp.view2 module', function() {
 
+    var $rootScope;
+
   beforeEach(module('myApp.view2'));
+
+    beforeEach(inject(function(_$rootScope_) {
+        $rootScope = _$rootScope_;
+    }));
 
   describe('view2 controller', function(){
 
     it('should ....', inject(function($controller) {
-      //spec body
-      var view2Ctrl = $controller('View2Ctrl');
-      expect(view2Ctrl).toBeDefined();
+        var $scope = {};
+
+        //spec body
+      var view2Ctrl = $controller('View2Ctrl', {$scope : $scope});
+        $scope.employees = [];
+        expect(view2Ctrl).toBeDefined();
+
     }));
 
   });
