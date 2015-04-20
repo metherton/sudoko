2490fc98b3eeb488ad3b69bd1d9fe24615e63d86
metherton <martinetherton63@googlemail.com>
2015-04-20 22:47:46 +0200

    added template

--- app/app.js
+++ app/app.js
@@ -1,12 +1,21 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+//var sudokoApp = angular.module('myApp', [
+//    'ngRoute',
+//    'myApp.view1',
+//    'myApp.view2',
+//    'myApp.version'
+//]);
+
+var sudokoApp = angular.module('myApp', ['ui.bootstrap']);
+
+//sudokoApp.config(['$routeProvider', function($routeProvider) {
+////  $routeProvider.otherwise({redirectTo: '/view1'});
+//  $routeProvider.otherwise({redirectTo: '/sudoko'});
+//}]);
+sudokoApp.directive('sudoko', function() {
+    return {
+        templateUrl: 'sudoko.html'
+    };
+});
