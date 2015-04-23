93a4d0c0779e38e1e6bee6996fc5de949ea2ca76
metherton <martinetherton63@googlemail.com>
2015-04-24 00:17:40 +0200

    added candidates for rows / columns / squares

--- app/app.js
+++ app/app.js
@@ -1,21 +1,10 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-//var sudokoApp = angular.module('myApp', [
-//    'ngRoute',
-//    'myApp.view1',
-//    'myApp.view2',
-//    'myApp.version'
-//]);
-
 var sudokoApp = angular.module('myApp', ['ui.bootstrap']);
 
-//sudokoApp.config(['$routeProvider', function($routeProvider) {
-////  $routeProvider.otherwise({redirectTo: '/view1'});
-//  $routeProvider.otherwise({redirectTo: '/sudoko'});
-//}]);
-
 sudokoApp.factory('Game', function() {
+
+    var self = this;
 
     var candidates = {
         rows: {
@@ -57,27 +46,42 @@ sudokoApp.factory('Game', function() {
         a2: '4',
         a5: '8',
         a7: '2',
+  //      b1: '5',
         b3: '8',
         b4: '2',
         b5: '6',
         b8: '9',
         c1: '7',
+   //     c5: '5', // 2
         c6: '4',
+   //     c7: '6', //12
         c9: '3',
+   //     d1: '8', //5
         d2: '3',
+   //     d3: '5', // 3
+   //     d5: '9', // 1
         d6: '2',
         d9: '4',
         e1: '1',
+  //      e2: '9',
+  //      e3: '4', // 7
         e5: '7',
         e7: '5',
+   //     f1: '2', //6
+   //     f2: '7', // 8
         f3: '6',
         f4: '5',
+   //     f5: '4', //4
+   //     f7: '8', //10
         f8: '3',
         g1: '9',
+  //      g3: '2', //11
         g4: '8',
         g7: '1',
         g8: '4',
+ //       h1: '3',//9
         h2: '6',
+ //       h5: '2', //12
         h6: '5',
         h7: '9',
         i1: '4',
@@ -85,6 +89,48 @@ sudokoApp.factory('Game', function() {
         i5: '1',
         i9: '5'
     };
+
+    var updateRowColumnsSquares = function() {
+        ['a','b','c','d','e','f','g','h','i'].forEach(function(row) {
+            ['1','2','3','4','5','6','7','8','9'].forEach(function(col) {
+                var key = row + col;
+                if (angular.isDefined(model[key])) {
+                    var rowIndex = candidates.rows[row].indexOf(model[key]);
+                    if (rowIndex > -1) {
+                        candidates.rows[row].splice(rowIndex,1);
+                    }
+                    var columnIndex = candidates.columns[col].indexOf(model[key]);
+                    if (columnIndex > -1) {
+                        candidates.columns[col].splice(columnIndex,1);
+                    }
+                    var convertRowColToKey = function convertRowColToKey() {
+                        var rowPart, colPart;
+                        if (['a','b','c'].indexOf(row) > -1) {
+                            rowPart = '1';
+                        } else if (['d','e','f'].indexOf(row) > -1) {
+                            rowPart = '2'
+                        } else {
+                            rowPart = '3';
+                        }
+                        if (['1','2','3'].indexOf(col) > -1) {
+                            colPart = '1';
+                        } else if (['4','5','6'].indexOf(col) > -1) {
+                            colPart = '2'
+                        } else {
+                            colPart = '3';
+                        }
+                        return rowPart + '_' + colPart;
+                    };
+                    var squareIndex = candidates.squares[convertRowColToKey()].indexOf(model[key]);
+                    if (squareIndex > -1) {
+                        candidates.squares[convertRowColToKey()].splice(squareIndex,1);
+                    }
+                }
+            })
+        });
+
+    };
+
 
     ['a','b','c','d','e','f','g','h','i'].forEach(function(row) {
         ['1','2','3','4','5','6','7','8','9'].forEach(function(col) {
@@ -95,56 +141,90 @@ sudokoApp.factory('Game', function() {
         })
     });
 
-    ['a','b','c','d','e','f','g','h','i'].forEach(function(row) {
-        ['1','2','3','4','5','6','7','8','9'].forEach(function(col) {
-            var key = row + col;
-            if (angular.isDefined(model[key])) {
-                var rowIndex = candidates.rows[row].indexOf(model[key]);
-                if (rowIndex > -1) {
-                    candidates.rows[row].splice(rowIndex,1);
-                }
-                var columnIndex = candidates.columns[col].indexOf(model[key]);
-                if (columnIndex > -1) {
-                    candidates.columns[col].splice(columnIndex,1);
-                }
-                var convertRowColToKey = function convertRowColToKey() {
-                    var rowPart, colPart;
-                    if (['a','b','c'].indexOf(row) > -1) {
-                        rowPart = '1';
-                    } else if (['d','e','f'].indexOf(row) > -1) {
-                        rowPart = '2'
-                    } else {
-                        rowPart = '3';
-                    }
-                    if (['1','2','3'].indexOf(col) > -1) {
-                        colPart = '1';
-                    } else if (['4','5','6'].indexOf(col) > -1) {
-                        colPart = '2'
-                    } else {
-                        colPart = '3';
-                    }
-                    return rowPart + '_' + colPart;
-                };
-                var squareIndex = candidates.squares[convertRowColToKey()].indexOf(model[key]);
-                if (squareIndex > -1) {
-                    candidates.squares[convertRowColToKey()].splice(squareIndex,1);
-                }
+    updateRowColumnsSquares();
+
+    var possibleValues = function(cell) {
+        if (model[cell] !== '') {
+            //    console.log('cell', cell, 'is undefined');
+            return undefined;
+        }
+        var row = cell.charAt(0);
+        var col = cell.charAt(1);
+        var convertRowColToKey = function convertRowColToKey() {
+            var rowPart, colPart;
+            if (['a','b','c'].indexOf(row) > -1) {
+                rowPart = '1';
+            } else if (['d','e','f'].indexOf(row) > -1) {
+                rowPart = '2'
+            } else {
+                rowPart = '3';
             }
-        })
-    });
+            if (['1','2','3'].indexOf(col) > -1) {
+                colPart = '1';
+            } else if (['4','5','6'].indexOf(col) > -1) {
+                colPart = '2'
+            } else {
+                colPart = '3';
+            }
+            return rowPart + '_' + colPart;
+        };
+        var cellCandidates = [];
+        ['1','2','3','4','5','6','7','8','9'].forEach(function(value) {
+            if (candidates.rows[row].indexOf(value) > -1 &&
+                candidates.columns[col].indexOf(value) > -1 &&
+                candidates.squares[convertRowColToKey()].indexOf(value) > -1) {
+                cellCandidates.push(value);
+            }
+        });
+        return cellCandidates;
+    };
+
 
     return {
+      set: function(newCell) {
+          model[newCell.cell] = newCell.value;
+          updateRowColumnsSquares();
+      },
       get: function() {
           return model;
       },
       getCandidates: function() {
           return candidates;
+      },
+      bestCandidate: function() {
+          var bestCandidateSoFar = {
+              cell: undefined,
+              numberOfCandidates: 9,
+              value: undefined
+          };
+          ['a','b','c','d','e','f','g','h','i'].forEach(function(row) {
+              ['1','2','3','4','5','6','7','8','9'].forEach(function(col) {
+                  var key = row + col;
+                  if (model[key] !== '') {
+                      return;
+                  }
+                  var candidatesForCell = possibleValues(key);
+
+                  if (bestCandidateSoFar.cell === undefined) {
+                      bestCandidateSoFar.cell = key;
+                      bestCandidateSoFar.numberOfCandidates = candidatesForCell.length;
+                      bestCandidateSoFar.value = candidatesForCell[0];
+                  } else {
+                      if (candidatesForCell.length < bestCandidateSoFar.numberOfCandidates) {
+                          bestCandidateSoFar.cell = key;
+                          bestCandidateSoFar.numberOfCandidates = candidatesForCell.length;
+                          bestCandidateSoFar.value = candidatesForCell[0];
+                      }
+                  }
+              })
+          });
+          return bestCandidateSoFar;
       }
     };
 
 });
 
-sudokoApp.directive('sudoko', function(Game) {
+sudokoApp.directive('sudoko', function(Game, $rootScope) {
 
     function getTemplate() {
         return '<div class="container-fluid"><button ng-click="solve()">Solve</button>' +
@@ -615,6 +695,14 @@ sudokoApp.directive('sudoko', function(Game) {
 
             $scope.solve = function() {
                 console.log('solve the puzzle');
+                for (var i = 0; i < 51; i++) {
+                    var newCell = Game.bestCandidate();
+                    console.log(newCell);
+
+                    Game.set(newCell);
+                    $scope.model = Game.get();
+
+                }
             };
 
             function initialize() {
